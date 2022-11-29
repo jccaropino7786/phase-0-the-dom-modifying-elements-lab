9504867f1d0f96fa7c0ad2859958a8992e18b6ac
john Caropino <jccaropino7786@live.com>
2022-11-29 07:52:10 -0800

    commit tree 378481e8f4d1a08694e15d8e586e215aea046b57

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+
+document.getElementById('main').remove()
+
+// function newHeader(){
+let newHeader = document.createElement('h1')
+const body = document.querySelector('body')
+body.prepend(newHeader)
+newHeader.id = 'victory'
+newHeader.innerHTML = "JC is the champion"
+// }
+// newHeader()
+
+
+// h1.id = 'victory'
+
+
+
+
+//the differnce between text content and innertext
